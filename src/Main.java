63773608977977793815034b2d63612c6677000d
Maxim <mr.maximgus@gmail.com>
2022-12-07 20:40:24 +0500

    origin commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] price = {13, 12, 10, 2, 31, 4};
+        SalesManager sale = new SalesManager(price);
+        int max = sale.max();
+        System.out.println("Максимальное значение - " + max);
+
+    }
+}
